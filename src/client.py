cd8e4cf21dba3596618eecf7a6160bf214a93f22
DJohn2315 <123595878+DJohn2315@users.noreply.github.com>
2026-02-11 09:49:19 -0600

    pre-merge with main

--- src/client.py
+++ src/client.py
@@ -1,28 +1,29 @@
 import socket
 import threading
+import queue
 
 HOST = "10.227.93.222"
 PORT = 12345
 
+_socket = None
+_recv_queue = queue.Queue()
+
 def recv_loop(sock: socket.socket):
-    """
-    Receive newline-delimited messages and print them.
-    """
     buf = b""
     try:
         while True:
             chunk = sock.recv(1024)
             if not chunk:
-                print("\n[Server disconnected]")
+                _recv_queue.put("\n[Server disconnected]")
                 break
 
             buf += chunk
             while b"\n" in buf:
                 line, buf = buf.split(b"\n", 1)
                 msg = line.decode("utf-8", errors="replace")
-                print(f"\nServer> {msg}\nYou> ", end="", flush=True)
+                _recv_queue.put(f"Server> {msg}")
     except OSError:
-        print("\n[Receive loop ended]")
+        _recv_queue.put("\n[Receive loop ended]")
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -49,6 +50,41 @@ def main():
             except OSError:
                 pass
 
+def connect(ip, port=PORT):
+    global _socket
+    try:
+        _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        _socket.connect((ip, PORT))
+        print(f"Connected to server at {ip}:{PORT}")
+
+        _socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+
+        t = threading.Thread(target=recv_loop, args=(_socket,), daemon=True)
+        t.start()
+        return f"Connection Successful"
+    except Exception as e:
+        return f"Error: {e}"
+
+def disconnect():
+    try:
+        print("Attempting Disconnect...")
+        _socket.shutdown(socket.SHUT_RDWR)
+        print("Successfully Disconnected!")
+    except Exception as e:
+        return f"Error: {e}"
+
+def get_message():
+    messages = []
+    while not _recv_queue.empty():
+        messages.append(_recv_queue.get_nowait())
+    return messages
+
+def send_message(msg):
+    try:
+        _socket.sendall((msg + "\n").encode("utf-8"))
+        _recv_queue.put(f"Client> {msg}")
+    except Exception as e:
+        return e
+
 if __name__ == "__main__":
     main()
-
